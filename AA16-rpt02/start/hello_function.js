0a90f3aeef51467c04be6e4288ce7a014d39d650
LSeungMOO <100826045+LSeungMOO@users.noreply.github.com>
2022-09-08 16:33:02 +0900

    Add files via upload

---
+++ AA16-rpt02/start/hello_function.js
@@ -0,0 +1,6 @@
+function hello(what){
+    console.log("Hello"+what+"!");
+}
+
+hello("aa16");
+hello("LSeungMoo, 이승무")
---
+++ AA16-rpt02/start/index_aann.js
@@ -0,0 +1,5 @@
+var myinfo = require('./aanninfo');
+
+myinfo("aa16", "LSeungMoo", '010-9002-2901');
+
+myinfo("aa00", "Redwoods", '010-1234-4678');
